1d2764eb55ef356e8e9da02a7cc3c76f400d4ecb
Ali Abu Harb <aliabuharb8530@gmail.com>
2020-10-21 10:06:10 +0300

    Downgrade the min sdk to 19

--- app/src/main/java/com/alex/sample/MainActivity.kt
+++ app/src/main/java/com/alex/sample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.alex.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       
     }
 }
